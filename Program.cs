2975bc7396089e4129c1a6e6a915342170d5d6ef
PshowGit <c.catanzaro@professionalshow.com>
2025-10-31 12:36:52 +0100

    First Commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdfPages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "args.txt");
+
+            if (args.Length > 0)
+            {
+                try
+                {
+                    File.WriteAllLines(path, args, Encoding.UTF8);
+
+                    Console.WriteLine("Argomenti ricevuti:");
+                    foreach (string arg in args)
+                    {
+                        Console.WriteLine(" - " + arg);
+                    }
+
+                    Console.WriteLine("\nArgomenti salvati in: " + path);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Errore durante il salvataggio: " + ex.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nessun argomento passato.");
+            }
+
+            Console.WriteLine("\nPremi un tasto per uscire...");
+            Console.ReadKey();
+        }
+    }
+}
